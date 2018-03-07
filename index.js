97a694620668adf6fa031ada8e754f576c53fcdc
vijay khatri <vijaykhatri22@gmail.com>
2018-03-07 19:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(recipes,key,value){
   return Object.assign({},recipes,{[key] : value});
 }
 
-function destructivelyUpadeObjectWithKeyAndValue(recipes,key,value){
+function destructivelyUpdateObjectWithKeyAndValue(recipes,key,value){
   recipes[key]=value;
   return recipes;
 }
